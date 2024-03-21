00dfece58a7627f4ffb06092f48b735909ba7bf6
Manuel Segimón Plana <manuelsegimon68@gmail.com>
2024-03-21 10:37:13 -0400

    extractContent to text extraction

---
+++ TextExtraction/extractContent.py
@@ -0,0 +1,14 @@
+from TextExtraction.cleanText import filter_content, generate_json_from_content
+from TextExtraction.webScrapper import scrape_article_content
+
+def extract_content(url):
+    # Scrape the article content
+    article_content = scrape_article_content(url)
+    
+    # Filter the content
+    filtered_content = filter_content(article_content)
+    
+    # Generate JSON from the filtered content
+    json_content = generate_json_from_content(filtered_content)
+    
+    return json_content
--- fileFunctions.py
+++ fileFunctions.py
@@ -2,20 +2,7 @@ import mysql.connector
 import json
 
 from db import getDBCursor, mydb
-from TextExtraction.cleanText import filter_content, generate_json_from_content
-from TextExtraction.webScrapper import scrape_article_content
-
-def extract_content(url):
-    # Scrape the article content
-    article_content = scrape_article_content(url)
-    
-    # Filter the content
-    filtered_content = filter_content(article_content)
-    
-    # Generate JSON from the filtered content
-    json_content = generate_json_from_content(filtered_content)
-    
-    return json_content
+from TextExtraction.extractContent import extract_content
 
 def upload_file(url, session_cookie):
     # Scrape the article content
