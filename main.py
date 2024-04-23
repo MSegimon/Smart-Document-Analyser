8888745c5ce2593ff345a6b8b3852417a58c7c99
Manuel Segimón Plana <manuelsegimon68@gmail.com>
2024-04-23 09:44:56 -0400

    Update uvicorn run host in main.py

--- main.py
+++ main.py
@@ -136,4 +136,4 @@ async def calculate_sentiment(analysis_request: AnalyseText):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
